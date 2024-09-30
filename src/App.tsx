90a96f6775ef1adb5977c8902253038ce0e1de0e
edthuman <edwardadirvine@aol.com>
2024-09-30 19:31:44 +0100

    update App to render a Login component inside a UserContext.Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { useState } from "react"
 import "./App.css"
+import Login from "./Login"
+import UserContext from "../hooks/UserContext.ts"
+import { User } from "../types.ts"
 
 function App() {
-  return <h1>Events Platform</h1>
+    const [user, setUser] = useState<User>({ role: "guest", username: "guest" })
+
+    return <UserContext.Provider value={user}>
+        <Login setUser={setUser}/>
+    </UserContext.Provider>
 }
 
 export default App
