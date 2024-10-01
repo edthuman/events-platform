0af3db2ba86e4a88e51e6f66adf71f2d6606b79f
edthuman <edwardadirvine@aol.com>
2024-10-01 18:41:05 +0100

    move establishing of connection to App.tsx, assiging it to FirebaseContext for reuse across the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
 import { useState } from "react"
 import "./App.css"
-import Login from "./Login"
 import UserContext from "../hooks/UserContext.ts"
+import FirebaseContext from "../hooks/FirebaseContext.ts"
 import { User } from "../types.ts"
-import Showings from "./Showings/Showings.tsx"
+import AppRouter from "./AppRouter.tsx"
+import { initializeApp } from "firebase/app"
+import { collection, getFirestore } from "@firebase/firestore"
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
+}
 
 function App() {
     const [user, setUser] = useState<User>({ role: "guest", username: "guest" })
+    const firebaseApp = initializeApp(firebaseConfig)
+    const firestore = getFirestore(firebaseApp)
+    const firestoreCollection = collection(firestore, "showings")
 
-    return <UserContext.Provider value={user}>
-        { user.role === "guest" ? <Login setUser={setUser}/> : <Showings /> }
-    </UserContext.Provider>
+    return user ? <UserContext.Provider value={{user, setUser}}>
+        <FirebaseContext.Provider value={firestoreCollection}>
+            <AppRouter/>
+        </FirebaseContext.Provider>
+    </UserContext.Provider> : null
 }
 
 export default App
--- src/Showings/Showings.tsx
+++ src/Showings/Showings.tsx
@@ -1,31 +1,18 @@
-import { collection, getDocs, getFirestore, query } from "@firebase/firestore";
-import { initializeApp } from "firebase/app";
-import { useEffect, useState } from "react";
+import { getDocs, query } from "@firebase/firestore";
+import { useContext, useEffect, useState } from "react";
 import ShowingCard from "./ShowingCard";
-
-const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID
-}
+import FirebaseContext from "../../hooks/FirebaseContext";
 
 function Showings() {
     const [showings, setShowings] = useState<any[]>([])
-    
-    async function getDocuments(){
-        const app = initializeApp(firebaseConfig)
-        const db = getFirestore(app)
+    const database = useContext(FirebaseContext)
 
-        const showingsRef = collection(db, "showings")
-        
-        const queryObject = query(showingsRef)
+    async function getDocuments(){        
+        const queryObject = query(database)
         const snapshot = await getDocs(queryObject)
 
         const fetchedShowings: any[] = []
-        snapshot.forEach((document)=>{
+        snapshot.forEach((document: any)=>{
             const showing = {
                 id: document.id,
                 ...document.data()
