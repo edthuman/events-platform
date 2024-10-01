6ce12fd1c97d7bbc15ffad3383cd2d20836162a9
edthuman <edwardadirvine@aol.com>
2024-10-01 19:20:45 +0100

    fix whitespace in AppRouter.tsx

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,28 +1,25 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Showings from './Showings/Showings';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Showings from "./Showings/Showings";
 import Login from "./Login";
 import SingleShowing from "./SingleShowing";
 
 const router = createBrowserRouter([
     {
-      path: "/",
-      element: <Login/>
+        path: "/",
+        element: <Login />,
     },
     {
-      path: "/showings",
-      element: <Showings/>
+        path: "/showings",
+        element: <Showings />,
     },
     {
         path: "/showing/:showing_id",
-        element: <SingleShowing/>
-      }
+        element: <SingleShowing />,
+    },
 ]);
 
 function AppRouter() {
-    return <RouterProvider router={router} />
+    return <RouterProvider router={router} />;
 }
 
-export default AppRouter
+export default AppRouter;
