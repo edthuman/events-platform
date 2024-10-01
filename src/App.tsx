29747fd91db1983c8c55908edbfcda3f43d70e3e
edthuman <edwardadirvine@aol.com>
2024-10-01 15:07:49 +0100

    add Showings component to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import "./App.css"
 import Login from "./Login"
 import UserContext from "../hooks/UserContext.ts"
 import { User } from "../types.ts"
+import Showings from "./Showings.tsx"
 
 function App() {
     const [user, setUser] = useState<User>({ role: "guest", username: "guest" })
 
     return <UserContext.Provider value={user}>
-        <Login setUser={setUser}/>
+        { user.role === "guest" ? <Login setUser={setUser}/> : <Showings /> }
     </UserContext.Provider>
 }
 
