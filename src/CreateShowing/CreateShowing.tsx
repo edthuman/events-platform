375d2884cb01adbc821f643bfec506da2e7a61ed
edthuman <edwardadirvine@aol.com>
2024-10-03 15:21:09 +0100

    update CreateShowing to not show film name form when film has been found

--- src/CreateShowing/CreateShowing.tsx
+++ src/CreateShowing/CreateShowing.tsx
@@ -9,8 +9,13 @@ function CreateShowing() {
     const [filmNameInput, setFilmNameInput] = useState("");
     const [filmDetails, setFilmDetails] = useState<{} | FoundFilmDetails>({});
     const [error, setError] = useState<string>("")
+    const [isSearchRequired, setIsSearchRequired] = useState(true)
     
-    return (
+    function handleFilmFound() {
+        setIsSearchRequired(false)
+    }
+
+    return isSearchRequired ? (
         <>
             <h1>Start A New Event!</h1>
             <p>Search by film name:</p>
@@ -27,13 +32,18 @@ function CreateShowing() {
             {Object.keys(filmDetails).length ? (
                 <>
                     <FilmPreview filmDetails={filmDetails}/>
-                    <p>Are these correct?</p>
-                    <button onClick={() => console.log("Yes")}>Yes</button>
+                    <p>Are these details correct?</p>
+                    <button onClick={handleFilmFound}>Yes</button>
                     <button onClick={() => console.log("No")}>No</button>
                 </>
             ) : null }
         </>
-    );
+    ) : (
+    <>
+        <h1>You are seeing:</h1>
+        <FilmPreview filmDetails={filmDetails} />
+    </>
+    )
 }
 
 export default CreateShowing;
