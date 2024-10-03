a829467adf193c0edb6fb43983e786f284080479
edthuman <edwardadirvine@aol.com>
2024-10-03 10:04:39 +0100

    add error message to Showings when no showings are retrieved from firebase

--- src/Showings/Showings.tsx
+++ src/Showings/Showings.tsx
@@ -5,27 +5,36 @@ import { Showing } from "../../types";
 import { getAllShowings } from "../../server/firestore-methods";
 
 function Showings() {
-    const [showings, setShowings] = useState<Showing[]>([])
-    const [isLoading, setIsLoading] = useState(true)
-    const firestore = useContext(FirebaseContext)
+    const [showings, setShowings] = useState<Showing[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const firestore = useContext(FirebaseContext);
 
-    useEffect(()=>{
-        (async()=>{
-            const fetchedShowings = await getAllShowings(firestore, setIsLoading)
-            setShowings(fetchedShowings)
-        })()
-    }, [])
-    
+    useEffect(() => {
+        (async () => {
+            const fetchedShowings = await getAllShowings(
+                firestore,
+                setIsLoading
+            );
+            setShowings(fetchedShowings);
+        })();
+    }, []);
+
     return isLoading ? (
         <>
             <h1>Loading showings...</h1>
         </>
+    ) : showings.length === 0 ? (
+        <>
+            <h1>Something went wrong whilst retrieving showings</h1>
+        </>
     ) : (
         <>
             <h1>Upcoming Showings</h1>
-            {showings.map((showing)=> <ShowingCard showing={showing} key={showing.id}/>)}
+            {showings.map((showing) => (
+                <ShowingCard showing={showing} key={showing.id} />
+            ))}
         </>
-    )
+    );
 }
 
-export default Showings
+export default Showings;
