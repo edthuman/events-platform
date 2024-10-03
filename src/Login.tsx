7cf156a9b7e8b971d7ed156cb8a81be308760dec
edthuman <edwardadirvine@aol.com>
2024-10-03 11:07:55 +0100

    update Login to use links instead of buttons for setting Staff and Non-Staff roles

--- src/Login.tsx
+++ src/Login.tsx
@@ -2,8 +2,8 @@ import { useContext } from "react"
 import UserContext from "../hooks/UserContext"
 
 function Login(){
-    const {user, setUser} = useContext(UserContext)
-    
+    const { setUser } = useContext(UserContext)
+
     const staffUser = {
         role: "staff",
         username: "test"
@@ -14,19 +14,11 @@ function Login(){
         username: "test"
     }
 
-    const guestUser = {
-        role: "guest",
-        username: "guest"
-    }
-
     return <>
         <h1>Events Platform</h1>
         <p>How would you like to log in?</p>
-        <button onClick={()=>setUser(staffUser)}>Staff</button>
-        <button onClick={()=>setUser(nonStaffUser)}>Non-Staff</button>
-        <p>You are {user.role === "guest" ? "not" : null} logged in {user.role !== "guest" ? `as ${user.role}` : null}</p>
-        {user.role !== "guest" ? <button onClick={()=>setUser(guestUser)}>Log out</button> : null}
-        <a href="/showings">See current showings</a>
+        <a href="/create-showing" onClick={()=>setUser(staffUser)} className="login-link">Staff</a>
+        <a href="/showings" onClick={()=>setUser(nonStaffUser)} className="login-link">Non-Staff</a>
     </>
 }
 
