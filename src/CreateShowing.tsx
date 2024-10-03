f999d32a5f8f875e6a868a78a1e1bb2d4d455c65
edthuman <edwardadirvine@aol.com>
2024-10-03 11:49:08 +0100

    add intial form to CreateShowing

--- src/CreateShowing.tsx
+++ src/CreateShowing.tsx
@@ -1,5 +1,28 @@
+import { useState } from "react"
+
 function CreateShowing() {
-    return <h1>Welcome to CreateShowing</h1>
+    const [filmNameInput, setFilmNameInput] = useState("")
+
+    function handleFilmNameInput(e: React.ChangeEvent<HTMLInputElement>) {
+        setFilmNameInput(e.target.value)
+    }
+
+    function findFilm(e: React.FormEvent<HTMLFormElement>){
+        e.preventDefault()
+    }
+
+    return <>
+        <h1>Start A New Event!</h1>
+        <form onSubmit={(e)=>findFilm(e)}>
+            <label>
+                    Which film will be showing?
+            </label>
+            <input type="text" onChange={(e)=>handleFilmNameInput(e)} value={filmNameInput}/>
+            <button type="submit">
+                Find Film
+            </button>
+        </form>
+    </>
 }
 
 export default CreateShowing
