27d4394966730f0244090beca401eb888365b391
edthuman <edwardadirvine@aol.com>
2024-10-04 17:09:31 +0100

    add initial getEventDetailsError function

--- src/CreateShowing/utils.ts
+++ src/CreateShowing/utils.ts
@@ -2,3 +2,7 @@ export function getCurrentDate() {
     const date = new Date().toLocaleDateString()
     return date.slice(-4) + "-" + date.slice(3, 5) + "-" + date.slice(0, 2)
 }
+
+export function getEventDetailsError(): string {
+    return ""
+}
