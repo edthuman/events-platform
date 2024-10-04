49127fc6c290436555dd256f202c905c8a430e29
edthuman <edwardadirvine@aol.com>
2024-10-04 13:09:43 +0100

    remove unused ShowingWithoutID type

--- server/firestore-types.ts
+++ server/firestore-types.ts
@@ -1,17 +1,13 @@
 import { Timestamp } from "@firebase/firestore"
 import { ErrorMessage } from "../types"
 
-export type ShowingWithoutID = {
+export type Showing = {
     name: string
     film: string
     description: string
     datetime: Timestamp
     poster: string
     imdbId: string
-}
-
-export interface Showing extends ShowingWithoutID {
-    id: string
 }
 
 export type SingleShowingResponse = Showing | ErrorMessage
