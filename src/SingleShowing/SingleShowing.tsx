d8f02ad596c01f9781b9a532f00014b5a7d1387d
edthuman <edwardadirvine@aol.com>
2024-10-04 12:55:41 +0100

    add comment to SingleShowing.tsx

--- src/SingleShowing/SingleShowing.tsx
+++ src/SingleShowing/SingleShowing.tsx
@@ -10,6 +10,7 @@ import ShowingDetails from "./ShowingDetails";
 const omdbKey = import.meta.env.VITE_OMDB_KEY;
 
 function SingleShowing() {
+    // TypeScript error on ShowingDetails component can be ignored - filmDetails will be of type FilmDetails whenever this renders
     const showingId = useParams().showing_id;
     const [showing, setShowing] = useState<any>(null);
     const [filmDetails, setFilmDetails] = useState<FilmDetailsResponse>({ error: "" });
