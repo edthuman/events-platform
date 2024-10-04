5d90da5cb1e644121ebd6c6340822f8a139c7c79
edthuman <edwardadirvine@aol.com>
2024-10-04 12:54:24 +0100

    use ShowingDetails in SingleShowing component

--- src/SingleShowing/SingleShowing.tsx
+++ src/SingleShowing/SingleShowing.tsx
@@ -1,11 +1,11 @@
 import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import FirebaseContext from "../../hooks/FirebaseContext";
-import { getDate, getTime } from "../../utils/datetime-utils";
 import { getSingleShowing } from "../../server/firestore-methods";
 import { getFilmDetails } from "../../server/omdb-methods";
 import "./SingleShowing.css";
 import { FilmDetailsResponse } from "../../server/omdb-types";
+import ShowingDetails from "./ShowingDetails";
 
 const omdbKey = import.meta.env.VITE_OMDB_KEY;
 
@@ -41,28 +41,7 @@ function SingleShowing() {
     ) : filmDetails.error ? (
         <h1>{filmDetails.error}</h1>
     ) : (
-        <>
-            <h1>{showing.name}</h1>
-            <p>{getDate(showing.datetime)}</p>
-            <p>{getTime(showing.datetime)}</p>
-            <img src={`${showing.poster}`} alt={`Poster for ${filmDetails.title}`} />
-            <p>{showing.description}</p>
-            <h2>Movie Details</h2>
-            <p>
-                {filmDetails.title} ({filmDetails.year})
-            </p>
-            <p>Rated: {filmDetails.rating}</p>
-            <p>Runtime: {filmDetails.runtime}</p>
-            <p>Directed by: {filmDetails.director}</p>
-            <p>Genre(s): {filmDetails.genre}</p>
-            <p>Plot: {filmDetails.plot}</p>
-            <a
-                href={`https://www.imdb.com/title/${showing.imdbId}`}
-                target="_blank"
-            >
-                Read more about the film on IMDb
-            </a>
-        </>
+        <ShowingDetails showing={showing} filmDetails={filmDetails}/>
     );
 }
 
