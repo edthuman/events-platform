75060e02c89e54228cdecc337a257edbd48328ae
edthuman <edwardadirvine@aol.com>
2024-10-05 10:42:32 +0100

    add logic to getEventDetailsError

--- src/CreateShowing/utils.ts
+++ src/CreateShowing/utils.ts
@@ -3,6 +3,12 @@ export function getCurrentDate(): string {
     return date.slice(-4) + "-" + date.slice(3, 5) + "-" + date.slice(0, 2)
 }
 
-export function getEventDetailsError(): string {
+export function getEventDetailsError(eventName: string, description: string): string {
+    if (!eventName) {
+        return "Must provide a name for your event"
+    }
+    if (!description) {
+        return "Must provide a description for your event"
+    }
     return ""
 }
