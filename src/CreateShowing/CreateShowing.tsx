b974fa848d8a216cdb5b82f805fe3024f4ddeb13
edthuman <edwardadirvine@aol.com>
2024-10-08 11:22:32 +0100

    add setting of UserContext user to CreateShowing

--- src/CreateShowing/CreateShowing.tsx
+++ src/CreateShowing/CreateShowing.tsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import FilmSearchForm from "./FilmSearchForm";
 import EventForm from "./EventForm";
 import { FilmPreviewResponse } from "../../server/omdb-types";
 import PostingSuccessMessage from "./PostingSuccessMessage";
 import Loading from "../Loading";
+import UserContext from "../../hooks/UserContext";
 
 function CreateShowing() {
     // TypeScript error on EventForm component can be ignored - isSearchRequired is only set to true when filmDetails are of type FilmPreviewDetails
@@ -11,6 +12,15 @@ function CreateShowing() {
     const [isSearchRequired, setIsSearchRequired] = useState(true)
     const [showingId, setShowingId] = useState("")
     const [isPosting, setIsPosting] = useState(false)
+    const { setUser } = useContext(UserContext);
+
+    useEffect(() => {
+        const staffUser = {
+            role: "staff",
+            username: "test"
+        }
+        setUser(staffUser)
+    }, [])
 
     return isSearchRequired ? (
         <FilmSearchForm filmDetails={filmDetails} setFilmDetails={setFilmDetails} setIsSearchRequired={setIsSearchRequired}/>
