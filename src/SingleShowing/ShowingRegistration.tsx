aaad550ebf78f7978ca209632e151f11fd69541f
edthuman <edwardadirvine@aol.com>
2024-10-08 16:48:12 +0100

    add isRegisteredState

--- src/SingleShowing/ShowingRegistration.tsx
+++ src/SingleShowing/ShowingRegistration.tsx
@@ -1,12 +1,14 @@
 import { useState } from "react"
 import RegisterButton from "./RegisterButton"
+import RegisteredMessage from "./RegisteredMessage"
 
 function ShowingRegistration({id}: {id: string}) {
     const [isError, setIsError] = useState(true)
+    const [isRegistered, setIsRegistered] = useState(false)
 
     return <>
         <p>Interested in attending?</p>
-        <RegisterButton setIsError={setIsError} id={id} />
+        {isRegistered ? <RegisteredMessage /> : <RegisterButton setIsError={setIsError} id={id} />}
         {isError ? <p>Error occurred during registration, please try again</p> : null}
     </>
 }
