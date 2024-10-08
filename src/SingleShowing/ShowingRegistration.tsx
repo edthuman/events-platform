190464a302b08ec33491041c955c4b87daf013b5
edthuman <edwardadirvine@aol.com>
2024-10-08 13:42:54 +0100

    add button to ShowingRegistration

--- src/SingleShowing/ShowingRegistration.tsx
+++ src/SingleShowing/ShowingRegistration.tsx
@@ -1,5 +1,15 @@
-function ShowingRegistration() {
-    return <p>Register for this event?</p>
+import { useState } from "react"
+import { Showing } from "../../server/firestore-types"
+import { handleRegistration } from "./event-handlers"
+
+function ShowingRegistration({showing}: {showing: Showing}) {
+    const [isButtonDisabled, setIsButtonDisabled] = useState(false)
+    const { id } = showing
+
+    return <>
+        <p>Interested in attending?</p>
+        <button onClick={() => handleRegistration(setIsButtonDisabled, id)} disabled={isButtonDisabled}>Register</button>
+    </>
 }
 
 export default ShowingRegistration
