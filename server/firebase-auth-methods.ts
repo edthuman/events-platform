72bbfd07b243df06588913c297e27c69e060d700
edthuman <edwardadirvine@aol.com>
2024-10-09 15:44:49 +0100

    add try catch blocks to getGoogleAuthorisation

--- server/firebase-auth-methods.ts
+++ server/firebase-auth-methods.ts
@@ -1,19 +1,24 @@
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 
 export async function getGoogleAuthorisation() {
-    const provider = new GoogleAuthProvider();
-    provider.addScope("https://www.googleapis.com/auth/calendar")
-
-    const auth = getAuth()
-
-    const result = await signInWithPopup(auth, provider)
-    const { email } = result.user
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
+    try {
+        const provider = new GoogleAuthProvider();
+        provider.addScope("https://www.googleapis.com/auth/calendar")
     
-    return { 
-        email, 
-        token, 
-        error: false 
+        const auth = getAuth()
+    
+        const result = await signInWithPopup(auth, provider)
+        const { email } = result.user
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        
+        return { 
+            email, 
+            token, 
+            error: false
+        }
+    }
+    catch (err) {
+        return { error: true }
     }
 }
