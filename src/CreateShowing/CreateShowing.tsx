419f30930f062da2161f67cbcd30cf697612d7d8
edthuman <edwardadirvine@aol.com>
2024-10-09 16:11:12 +0100

    remove use of setUser from CreateShowing

--- src/CreateShowing/CreateShowing.tsx
+++ src/CreateShowing/CreateShowing.tsx
@@ -1,10 +1,9 @@
-import { useContext, useEffect, useState } from "react";
+import { useState } from "react";
 import FilmSearchForm from "./FilmSearchForm";
 import EventForm from "./EventForm";
 import { FilmPreviewResponse } from "../../server/omdb-types";
 import PostingSuccessMessage from "./PostingSuccessMessage";
 import Loading from "../Loading";
-import UserContext from "../../hooks/UserContext";
 
 function CreateShowing() {
     // TypeScript error on EventForm component can be ignored - isSearchRequired is only set to true when filmDetails are of type FilmPreviewDetails
@@ -12,15 +11,6 @@ function CreateShowing() {
     const [isSearchRequired, setIsSearchRequired] = useState(true)
     const [showingId, setShowingId] = useState("")
     const [isPosting, setIsPosting] = useState(false)
-    const { setUser } = useContext(UserContext);
-
-    useEffect(() => {
-        const staffUser = {
-            role: "staff",
-            username: "test"
-        }
-        setUser(staffUser)
-    }, [])
 
     return isSearchRequired ? (
         <FilmSearchForm filmDetails={filmDetails} setFilmDetails={setFilmDetails} setIsSearchRequired={setIsSearchRequired}/>
