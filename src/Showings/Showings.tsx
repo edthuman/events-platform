caf658446eda2d6af0691716daf5ee362ee0975e
edthuman <edwardadirvine@aol.com>
2024-10-09 16:11:50 +0100

    remove use of setUser from Showings

--- src/Showings/Showings.tsx
+++ src/Showings/Showings.tsx
@@ -4,22 +4,14 @@ import FirebaseContext from "../../hooks/FirebaseContext";
 import { getAllShowings } from "../../server/firestore-methods";
 import { Showing } from "../../server/firestore-types";
 import Loading from "../Loading";
-import UserContext from "../../hooks/UserContext";
 
 function Showings() {
     const [showings, setShowings] = useState<Showing[]>([]);
     const [isLoading, setIsLoading] = useState(true);
     const firestore = useContext(FirebaseContext);
-    const { setUser } = useContext(UserContext)
 
     useEffect(() => {
         (async () => {
-            const nonStaffUser = {
-                role: "non-staff",
-                username: "test"
-            }
-            setUser(nonStaffUser)
-
             const fetchedShowings = await getAllShowings(
                 firestore,
                 setIsLoading
