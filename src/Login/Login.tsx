a9f6296f9149ca2bb4918b4eae59832efb3ee028
edthuman <edwardadirvine@aol.com>
2024-10-09 15:51:10 +0100

    update Login to use handleLogin function

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,27 +1,39 @@
 import { Link } from "react-router-dom";
 import "./Login.css"
+import { useContext } from "react";
+import UserContext from "../../hooks/UserContext";
+import { handleLogin } from "./event-handlers";
 
 function Login() {
-    return (
+    const {user, setUser} = useContext(UserContext)
+    
+    return !user.email ? (
         <>
             <h1>Events Platform</h1>
-            <p>How would you like to log in?</p>
-            <div id="login-links">
+            <button onClick={() => handleLogin(setUser, "staff")}>
+                Staff log in
+            </button>
+            <button onClick={() => handleLogin(setUser, "non-staff")}>
+                Non-Staff log in
+            </button>
+        </>
+    ) : <>
+    <h1>Login Successful</h1>
+    <div id="login-links">
                 <Link
                     to="/create-showing"
                     className="login-link"
                 >
-                    Staff
+                    Create Showing
                 </Link>
                 <Link
                     to="/showings"
                     className="login-link"
                 >
-                    Non-Staff
+                    View Showings
                 </Link>
             </div>
-        </>
-    );
+    </>
 }
 
 export default Login;
