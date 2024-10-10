9e074f71fb2ab55135e66531d8acc874bf9dc29e
edthuman <edwardadirvine@aol.com>
2024-10-10 19:26:57 +0100

    add message explaining how to find an IMDb ID

--- src/CreateShowing/FilmSearch/SearchForm.tsx
+++ src/CreateShowing/FilmSearch/SearchForm.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { BooleanStateSetter, SetFilmDetails } from "../../../types"
 import { findFilmDetails, handleTextInput } from "../event-handlers"
+import { Link } from "react-router-dom";
 
 function SearchForm({setFilmDetails, omdbKey, setIsLoading} : {setFilmDetails: SetFilmDetails, omdbKey: string, setIsLoading: BooleanStateSetter}) {
     const [isNameSearch, setIsNameSearch] = useState(true)
@@ -8,6 +9,11 @@ function SearchForm({setFilmDetails, omdbKey, setIsLoading} : {setFilmDetails: S
 
     return <>
         <p>Search by {isNameSearch ? "name" : "IMDb ID"}:</p>
+        {isNameSearch ? (
+            null
+        ) : (
+            <p>An IMDb ID can be found by searching a film on <Link to="https://www.imdb.com" target="_blank">imdb.com</Link>, then copying the part of the URL just after title (between the two slashes) which begins 'tt'</p>
+        )}
         <form
             onSubmit={(e) =>
                 findFilmDetails(
