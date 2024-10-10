4528987ed6a954c74e958a5a3dcb9f3727a6a59d
edthuman <edwardadirvine@aol.com>
2024-10-10 17:30:31 +0100

    rename PostingSuccessMessage to PostSuccessMessage

--- src/CreateShowing/PostSuccessMessage.tsx
+++ src/CreateShowing/PostSuccessMessage.tsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom"
 
-function PostingSuccessMessage({showingId}: {showingId: string}) {
+function PostSuccessMessage({showingId}: {showingId: string}) {
     return <>
         <h1>Event Posted!</h1>
         <Link to={`/showing/${showingId}`} target="_self">See event page</Link>
     </>
 }
 
-export default PostingSuccessMessage
+export default PostSuccessMessage
