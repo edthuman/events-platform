f8f959153918fc263c607d4fc55de811ca49e8e4
edthuman <edwardadirvine@aol.com>
2024-10-10 17:27:39 +0100

    fix link in PostingSuccessMessage

--- src/CreateShowing/PostingSuccessMessage.tsx
+++ src/CreateShowing/PostingSuccessMessage.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 function PostingSuccessMessage({showingId}: {showingId: string}) {
     return <>
         <h1>Event Posted!</h1>
-        <Link to={`showing/${showingId}`} target="_self">See event page</Link>
+        <Link to={`/showing/${showingId}`} target="_self">See event page</Link>
     </>
 }
 
