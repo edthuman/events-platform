e4fa14d5d54a4acec273e564544526347df99b60
edthuman <edwardadirvine@aol.com>
2024-10-10 13:43:16 +0100

    add use of isNotInCalendar state to RegisteredMessage

--- src/SingleShowing/AttendShowing.tsx
+++ src/SingleShowing/AttendShowing.tsx
@@ -1,26 +1,39 @@
-import { useContext, useState } from "react"
-import UserContext from "../../hooks/UserContext"
+import { useContext, useState } from "react";
+import UserContext from "../../hooks/UserContext";
 import { Showing } from "../../server/firestore-types";
 import ShowingRegistration from "./ShowingRegistration";
 import RegisteredMessage from "./RegisteredMessage";
+import { BooleanStateSetter } from "../../types";
 
-function AttendShowing({showing}: {showing: Showing}) {
-    const { user } = useContext(UserContext)
-    const isUserInAttendees = showing.attendees.includes(user.email)
-    const [isUserAttending, setIsUserAttending] = useState(isUserInAttendees)
+function AttendShowing({
+    showing,
+    isNotInCalendar,
+    setIsNotInCalendar,
+}: {
+    showing: Showing;
+    isNotInCalendar: boolean;
+    setIsNotInCalendar: BooleanStateSetter;
+}) {
+    const { user } = useContext(UserContext);
+    const isUserInAttendees = showing.attendees.includes(user.email);
+    const [isUserAttending, setIsUserAttending] = useState(isUserInAttendees);
 
     return user.role === "guest" ? (
         <p>Please log in to register for event</p>
+    ) : user.role === "staff" ? (
+        <p>You must be non-staff to register for an event</p>
+    ) : isUserAttending ? (
+        <RegisteredMessage
+            showing={showing}
+            isNotInCalendar={isNotInCalendar}
+            setIsNotInCalendar={setIsNotInCalendar}
+        />
     ) : (
-        user.role === "staff" ? (
-            <p>You must be non-staff to register for an event</p>
-        ): (
-            isUserAttending ? (
-                <RegisteredMessage showing={showing} token={user.token}/>
-            ) : (
-                <ShowingRegistration id={showing.id} setIsUserAttending={setIsUserAttending}/>
-        ))
-    )
+        <ShowingRegistration
+            id={showing.id}
+            setIsUserAttending={setIsUserAttending}
+        />
+    );
 }
 
-export default AttendShowing
+export default AttendShowing;
--- src/SingleShowing/RegisteredMessage.tsx
+++ src/SingleShowing/RegisteredMessage.tsx
@@ -1,18 +1,39 @@
-import { useState } from "react"
-import { Showing } from "../../server/firestore-types"
-import { handleAddToCalendarClick } from "./event-handlers"
+import { useContext } from "react";
+import { Showing } from "../../server/firestore-types";
+import { handleAddToCalendarClick } from "./event-handlers";
+import UserContext from "../../hooks/UserContext";
+import { BooleanStateSetter } from "../../types";
 
-function RegisteredMessage({showing, token}: {showing: Showing, token: string}) {
-    const [isNotInCalendar, setIsNotInCalender] = useState(true)
-    
-    return <>
-        <p>You are registered for this event</p>
-        {isNotInCalendar ? (
-            <button onClick={() => handleAddToCalendarClick(showing, token, setIsNotInCalender)}>
-                Add to Google Calendar
-            </button>
-        ) : null}
-    </>
+function RegisteredMessage({
+    showing,
+    isNotInCalendar,
+    setIsNotInCalendar,
+}: {
+    showing: Showing;
+    isNotInCalendar: boolean;
+    setIsNotInCalendar: BooleanStateSetter;
+}) {
+    const {
+        user: { token },
+    } = useContext(UserContext);
+    return (
+        <>
+            <p>You are registered for this event</p>
+            {isNotInCalendar ? (
+                <button
+                    onClick={() =>
+                        handleAddToCalendarClick(
+                            showing,
+                            token,
+                            setIsNotInCalendar
+                        )
+                    }
+                >
+                    Add to Google Calendar
+                </button>
+            ) : null}
+        </>
+    );
 }
 
-export default RegisteredMessage
+export default RegisteredMessage;
