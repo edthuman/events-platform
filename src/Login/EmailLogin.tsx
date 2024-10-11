c53db1cc218e1b210672f05287418fc099fed4b5
edthuman <edwardadirvine@aol.com>
2024-10-11 18:40:19 +0100

    add role radio buttons to EmailLogin

--- src/Login/EmailLogin.tsx
+++ src/Login/EmailLogin.tsx
@@ -1,21 +1,23 @@
 import { useContext, useState } from "react";
 import UserContext from "../../hooks/UserContext";
 import EmailFormElements from "./EmailFormElements";
-import { handleEmailLogin } from "./event-handlers";
+import { handleEmailLogin, handleRoleInputChange } from "./event-handlers";
 
 function EmailLogin() {
     const { setUser } = useContext(UserContext);
     const [error, setError] = useState("");
+    const [roleInput, setRoleInput] = useState("staff")
 
     return <>
         {error ? <p>{error}</p> : null}
-        <form id="sign-up-form" onSubmit={(e) => handleEmailLogin(e, setUser, setError)}>
+        <form id="sign-up-form" onSubmit={e => handleEmailLogin(e, setUser, setError)}>
+            <label htmlFor="staff" >Staff</label>
+            <input type="radio" id="staff" name="role" value="staff" checked={roleInput === "staff"} onChange={e => handleRoleInputChange(e, setRoleInput)}/>
+            <label htmlFor="non-staff">Non-Staff</label>
+            <input type="radio" id="non-staff" name="role" value="non-staff" checked={roleInput === "non-staff"} onChange={e => handleRoleInputChange(e, setRoleInput)}/>
             <EmailFormElements />
             <button type="submit">
-                Sign in as Staff
-            </button>
-            <button type="submit">
-                Sign in as Non-staff
+                Sign in
             </button>
         </form>
     </>
