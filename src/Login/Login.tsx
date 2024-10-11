6ba2a08c026f05179f94d3655fb7b2fb5a1ee163
edthuman <edwardadirvine@aol.com>
2024-10-11 13:48:13 +0100

    add sign up button to Login

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,24 +1,30 @@
 import "./Login.css"
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import UserContext from "../../hooks/UserContext";
-import { handleLogin } from "./event-handlers";
+import { handleLogin, handleSignUpClick } from "./event-handlers";
 import SuccessfulLoginLinks from "./SuccessfulLoginLinks";
+import SignUpForm from "./SignUpForm";
 
 function Login() {
     const {user, setUser} = useContext(UserContext)
+    const [isSigningUp, setIsSigningUp] = useState(false)
 
     return <>
         <h1>Events Platform</h1>
         {!user.email ? (
-            <>
-                
-                <button onClick={() => handleLogin(setUser, "staff")}>
-                    Staff log in
-                </button>
-                <button onClick={() => handleLogin(setUser, "non-staff")}>
-                    Non-Staff log in
-                </button>
-            </>
+            isSigningUp ? (
+                <SignUpForm />
+            ) : (
+                <>   
+                    <button onClick={() => handleLogin(setUser, "staff")}>
+                        Staff log in
+                    </button>
+                    <button onClick={() => handleLogin(setUser, "non-staff")}>
+                        Non-Staff log in
+                    </button>
+                    <button onClick={()=>setIsSigningUp(true)}>Sign up</button>
+                </>
+            )
         ) : (
             <SuccessfulLoginLinks user={user}/>
         )}
