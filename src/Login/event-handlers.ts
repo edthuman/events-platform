159be8d1aa84446e15090ffc7da81748ecb6d800
edthuman <edwardadirvine@aol.com>
2024-10-11 14:31:51 +0100

    add initial handleSignUpClick event handler

--- src/Login/event-handlers.ts
+++ src/Login/event-handlers.ts
@@ -1,3 +1,4 @@
+import { createUser } from "../../server/firestore-methods";
 import { getGoogleAuthorisation } from "../../server/google-methods";
 
 export async function handleLogin(
@@ -8,4 +9,19 @@ export async function handleLogin(
         setUser({ ...response, role });
         return;
     });
+}
+
+export async function handleSignUpClick(e: React.FormEvent<HTMLFormElement>, setUser: React.SetStateAction<any>) {
+    e.preventDefault()
+    
+    const {elements} = e.target
+    
+    const email = elements.email.value
+    const password = elements.password.value
+
+    const response = await createUser(email, password)
+    setUser({
+        role: "guest",
+        email: response.email
+    })
 }
