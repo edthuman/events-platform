9ed94b7bfffa51c287038350e6b8ad7dd8829898
edthuman <edwardadirvine@aol.com>
2024-10-11 14:48:05 +0100

    add error state to SignUpForm

--- src/Login/SignUpForm.tsx
+++ src/Login/SignUpForm.tsx
@@ -8,15 +8,18 @@ function SignUpForm() {
     const { setUser } = useContext(UserContext)
     const [emailInput, setEmailInput] = useState("")
     const [passwordInput, setPasswordInput] = useState("")
+    const [error, setError] = useState("")
 
-    return setUser ? (
-        <form id="sign-up-form" onSubmit={e => handleSignUpClick(e, setUser)}>
+    return setUser ? (<>
+        {error ? <p>{error}</p> : null}
+        <form id="sign-up-form" onSubmit={e => handleSignUpClick(e, setUser, setError)}>
             <label htmlFor="email">Email:</label>
             <input id="email" type="email" value={emailInput} onChange={e => handleTextInput(e, setEmailInput)}/>
             <label htmlFor="password">Password:</label>
             <input id="password" type="password" value={passwordInput} onChange={e => handleTextInput(e, setPasswordInput)}/>
             <button type="submit">Sign Up</button>
         </form>
+    </>
     )
     : null
 }
