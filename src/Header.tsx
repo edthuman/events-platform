32875fe89dcc01698eb9c87cb617845f09ad028b
edthuman <edwardadirvine@aol.com>
2024-10-12 21:04:13 +0100

    add styling for Header Link

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,11 +1,12 @@
 import { Link, useLocation } from "react-router-dom"
+import "./Header.css"
 
 function Header() {
     const {pathname} = useLocation()
 
     return <>
         <h1>Community Cinema</h1>
-        {pathname !== "/login" ? <Link to="/login">Login</Link> : null}
+        {pathname !== "/login" ? <Link to="/login" id="login-button">Login</Link> : null}
     </>
 }
 
