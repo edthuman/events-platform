c5a9da2414ffca0c1f41c75bae2afcc44ed9ea67
edthuman <edwardadirvine@aol.com>
2024-10-16 15:08:53 +0100

    update CheckoutForm to add showing query to redirect link

--- src/SingleShowing/CheckoutForm.tsx
+++ src/SingleShowing/CheckoutForm.tsx
@@ -29,7 +29,7 @@ export default function CheckoutForm({ showing }: { showing: Showing }) {
         const { error } = await stripe.confirmPayment({
             elements,
             confirmParams: {
-                return_url: "http://localhost:5173/complete",
+                return_url: `http://localhost:5173/complete?showing=${showing.id}`,
             },
             redirect: undefined,
         });
