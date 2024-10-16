63dfea0dc8d131c63fb0eff01c447582b37215c5
edthuman <edwardadirvine@aol.com>
2024-10-16 13:30:17 +0100

    update AppRouter to render a PaymentComplete Page on /complete endpoint

--- src/Page.tsx
+++ src/Page.tsx
@@ -1,25 +1,34 @@
 import CreateShowing from "./CreateShowing/CreateShowing";
 import Header from "./Header";
 import Login from "./Login/Login";
+import PaymentComplete from "./PaymentComplete/PaymentComplete";
 import Showings from "./Showings/Showings";
 import SingleShowing from "./SingleShowing/SingleShowing";
 
-type Page = "Login" | "Showings" | "SingleShowing" | "CreateShowing" 
+type Page =
+    | "Login"
+    | "Showings"
+    | "SingleShowing"
+    | "CreateShowing"
+    | "PaymentComplete";
 
 function Page({ page }: { page: Page }) {
     const pages = {
         Login,
         Showings,
         SingleShowing,
-        CreateShowing
-    }
+        CreateShowing,
+        PaymentComplete
+    };
 
-    const CurrentPage = pages[page]
+    const CurrentPage = pages[page];
 
-    return <>
-        <Header />
-        <CurrentPage />
-    </>
+    return (
+        <>
+            <Header />
+            <CurrentPage />
+        </>
+    );
 }
 
-export default Page
+export default Page;
