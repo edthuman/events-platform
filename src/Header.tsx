9f4c9034cbe84d8c4c2df44785c0def0ca125fef
edthuman <edwardadirvine@aol.com>
2024-10-17 13:29:13 +0100

    update Header to not display login button to logged in users

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,12 +1,16 @@
 import { Link, useLocation } from "react-router-dom"
 import "./Header.css"
+import UserContext from "../hooks/UserContext"
+import { useContext } from "react"
 
 function Header() {
-    const {pathname} = useLocation()
+    const { pathname } = useLocation()
+    const { user } = useContext(UserContext)
+    const isLoginLinkRequired = pathname !== "/login" && !user.email
 
     return <>
         <h1>Community Cinema</h1>
-        {pathname !== "/login" ? <Link to="/login" id="login-button">Login</Link> : null}
+        {isLoginLinkRequired ? <Link to="/login" id="login-button">Login</Link> : null}
     </>
 }
 
