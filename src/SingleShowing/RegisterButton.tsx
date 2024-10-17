f3287f1f20ca0425c816b1e28a8a80e169c2b835
edthuman <edwardadirvine@aol.com>
2024-10-17 13:09:09 +0100

    update isError boolean state to be error string statein RegisterButton and ShowingRegistration

--- src/SingleShowing/RegisterButton.tsx
+++ src/SingleShowing/RegisterButton.tsx
@@ -2,10 +2,10 @@ import { useContext, useState } from "react"
 import { handleBuyTicketClick } from "./event-handlers"
 import UserContext from "../../hooks/UserContext"
 import FirebaseContext from "../../hooks/FirebaseContext"
-import { BooleanStateSetter } from "../../types"
+import { BooleanStateSetter, StringStateSetter } from "../../types"
 import DonationForm from "./DonationForm"
 
-function RegisterButton({setIsError, setIsUserAttending, id, setIsPaying, price, donation, setDonation} : {setIsError: BooleanStateSetter, setIsUserAttending: BooleanStateSetter, id: string, setIsPaying: BooleanStateSetter, price: "any" | number, donation: string, setDonation: string}) {
+function RegisterButton({setError, setIsUserAttending, id, setIsPaying, price, donation, setDonation} : {setError: StringStateSetter, setIsUserAttending: BooleanStateSetter, id: string, setIsPaying: BooleanStateSetter, price: "any" | number, donation: string, setDonation: StringStateSetter}) {
     const { user: { email } } = useContext(UserContext)
     const [isButtonDisabled, setIsButtonDisabled] = useState(false)
     const firebase = useContext(FirebaseContext)
@@ -14,7 +14,7 @@ function RegisterButton({setIsError, setIsUserAttending, id, setIsPaying, price,
     return <>
         <p>Interested in attending?</p>
         {isAnyPrice ? <DonationForm donation={donation} setDonation={setDonation}/> : null}
-        <button onClick={() => handleBuyTicketClick(setIsPaying, setIsButtonDisabled, setIsError, setIsUserAttending, firebase, email, id, price, donation)} disabled={isButtonDisabled}>
+        <button onClick={() => handleBuyTicketClick(setIsPaying, setIsButtonDisabled, setError, setIsUserAttending, firebase, email, id, price, donation)} disabled={isButtonDisabled}>
             Buy Ticket
         </button>
     </>
--- src/SingleShowing/ShowingRegistration.tsx
+++ src/SingleShowing/ShowingRegistration.tsx
@@ -4,12 +4,12 @@ import { BooleanStateSetter, StringStateSetter } from "../../types"
 import { Showing } from "../../server/firestore-types"
 
 function ShowingRegistration({showing, setIsUserAttending, setIsPaying, donation, setDonation}: {showing: Showing, setIsUserAttending: BooleanStateSetter, setIsPaying: BooleanStateSetter, donation: string, setDonation: StringStateSetter}) {
-    const [isError, setIsError] = useState(false)
+    const [error, setError] = useState("")
     const { id, price } = showing
-    
+    console.log(setDonation)
     return <>
-        <RegisterButton setIsError={setIsError} setIsUserAttending={setIsUserAttending} id={id} setIsPaying={setIsPaying} price={price} donation={donation} setDonation={setDonation}/>
-        {isError ? <p>Error occurred during registration, please try again</p> : null}
+        <RegisterButton setError={setError} setIsUserAttending={setIsUserAttending} id={id} setIsPaying={setIsPaying} price={price} donation={donation} setDonation={setDonation}/>
+        {error ? <p>{error}</p> : null}
     </>
 }
 
