241ca7bc31a12bfd9767693fcdf970918ac2b54d
edthuman <edwardadirvine@aol.com>
2024-10-18 11:10:18 +0100

    refactor Login component to ask role type before login method

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -3,34 +3,45 @@ import { useContext, useState } from "react";
 import UserContext from "../../hooks/UserContext";
 import SuccessfulLoginLinks from "./SuccessfulLoginLinks";
 import SignUpForm from "./SignUpForm";
-import { handleGoogleLoginClick } from "./event-handlers";
-import GoogleLogin from "./GoogleLogin";
+import { handleGoogleLogin } from "./event-handlers";
 import EmailLogin from "./EmailLogin";
+import { User } from "../../types"
 
 function Login() {
-    const { user } = useContext(UserContext)
+    const { user, setUser } = useContext(UserContext)
     const [isSigningUp, setIsSigningUp] = useState(false)
-    const [isLoggingIn, setIsLoggingIn] = useState(false)
-    const [loginType, setLoginType] = useState("email")
+    const [isEmailLogin, setIsEmailLogin] = useState(false)
+    
+    function updateUserRole(newRole: "non-staff" | "staff") {
+        setUser((currUser: User) => {
+            const newUser = {...currUser}
+            newUser.role = newRole
+            return newUser
+        })
+    }
 
-    return !user.email ? (
+    return user.email ? (
+        <SuccessfulLoginLinks user={user}/>
+    ) :(
+        user.role === "guest" ? (
+        <>
+            <p>How are you logging in today?</p>
+            <button onClick={() => updateUserRole("staff")}>Staff</button>
+            <button onClick={() => updateUserRole("non-staff")}>Non-staff</button>
+        </>
+    ) : (
         isSigningUp ? (
             <SignUpForm />
         ) : (
-            isLoggingIn ? (
-                loginType === "google" ? (
-                    <GoogleLogin />
-                ) : (
-                    <EmailLogin />
-                )
+            isEmailLogin ? (
+                <EmailLogin />
             ) : (
-                <>
-                    <button onClick={() => setIsSigningUp(true)}>Sign up</button>
-                    <button onClick={() => handleGoogleLoginClick(setIsLoggingIn, setLoginType)}>Log in with Google account</button>
-                    <button onClick={() => setIsLoggingIn(true)}>Log in with Email</button>
-                </>
-        ))) : (
-            <SuccessfulLoginLinks user={user}/>
+        <>
+            <button onClick={() => setIsEmailLogin(true)}>Sign in with Email</button>
+            <button onClick={() => handleGoogleLogin(setUser, user.role)}>Sign in with Google</button>
+            <button onClick={() => setIsSigningUp(true)}>Sign up</button>
+        </> 
+        )))
     )
 }
 
