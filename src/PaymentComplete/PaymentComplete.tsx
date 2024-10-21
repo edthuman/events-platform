bd6886d6f2190fdc95fc26054b23073e6838585c
edthuman <edwardadirvine@aol.com>
2024-10-21 14:37:09 +0100

    add setting of user in PaymentComplete

--- src/PaymentComplete/PaymentComplete.tsx
+++ src/PaymentComplete/PaymentComplete.tsx
@@ -1,12 +1,24 @@
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import PaymentResponse from "./PaymentResponse";
+import { useContext, useEffect } from "react";
+import UserContext from "../../hooks/UserContext";
 
 const stripePromise = loadStripe(
     "pk_test_51Q9nghKK7ykYcCh0ywwIIxPTONKVi1uxYPrboXY4LQfSGkIs9Mj0vSwWsmGeFhYMtwDhCk8B0ZWsZKXdVcXBjSYQ00juMM9MCx"
 );
 
 function PaymentComplete() {
+    const { setUser } = useContext(UserContext)
+
+    useEffect(() => {
+        const userJSON = window.sessionStorage.getItem("user")
+        if (userJSON) {
+            const user = JSON.parse(userJSON)
+            setUser(user)
+        }
+    }, [])
+    
     return (
         <Elements stripe={stripePromise}>
             <PaymentResponse />
