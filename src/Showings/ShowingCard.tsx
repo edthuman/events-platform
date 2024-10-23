ec4c0e34c317316a62c744dc1b1faf15b7dd77a3
edthuman <edwardadirvine@aol.com>
2024-10-23 14:47:27 +0100

    add initial ShowingCard styling

--- src/Showings/ShowingCard.tsx
+++ src/Showings/ShowingCard.tsx
@@ -1,19 +1,16 @@
-import "./ShowingCard.css"
-import { getDate, getTime } from "../../utils/datetime-utils"
+import { getDate } from "../../utils/datetime-utils"
 import { Showing } from "../../server/firestore-types"
 import { Link } from "react-router-dom"
 
 function ShowingCard({showing}: { showing: Showing }) {
     const {startDate} = showing
     const day = getDate(startDate)
-    const time = getTime(startDate)
 
     return <Link to={`/showing/${showing.id}`} target="_self">
-        <div className="showing-card" >
+        <div className="mb-10 flex flex-col items-center py-4 bg-[#195477] hover:bg-[#10374e]">
+            <img src={`${showing.poster}`} alt={`Movie post for ${showing.film}`} className="size-10/12"/>
             <p>{showing.name}</p>
             <p>{day}</p>
-            <p>{time}</p>
-            <img src={`${showing.poster}`} alt={`Movie post for ${showing.film}`} className="poster"/>
         </div>
     </Link>
 }
