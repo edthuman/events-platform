16f47bf46eeca4abace95568fcd45328fc23999a
edthuman <edwardadirvine@aol.com>
2024-10-24 15:22:38 +0100

    remove Form.css use from EmailLogin and SignUpForm

--- src/Login/EmailLogin.tsx
+++ src/Login/EmailLogin.tsx
@@ -2,16 +2,15 @@ import { useContext } from "react";
 import UserContext from "../../hooks/UserContext";
 import EmailFormElements from "./EmailFormElements";
 import { handleEmailLogin } from "./event-handlers";
-import "./Form.css"
 import { StringStateSetter } from "../../types";
 
 function EmailLogin({ setError }: { setError: StringStateSetter }) {
     const { setUser } = useContext(UserContext);
 
     return <>
-        <form className="sign-up-form" onSubmit={e => handleEmailLogin(e, setUser, setError)}>
+        <form className="flex flex-col" onSubmit={e => handleEmailLogin(e, setUser, setError)}>
             <EmailFormElements />
-            <button type="submit">
+            <button type="submit" className="border w-20 mx-auto mt-5 hover:text-grey">
                 Sign in
             </button>
         </form>
