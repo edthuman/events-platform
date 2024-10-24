77e651e08ee3eb3d4657ca75496db805d92ca94b
edthuman <edwardadirvine@aol.com>
2024-10-24 11:09:38 +0100

    adjust spacing in RegisteredMessage

--- src/SingleShowing/RegisteredMessage.tsx
+++ src/SingleShowing/RegisteredMessage.tsx
@@ -13,7 +13,7 @@ function RegisteredMessage({
 
     return (
         <>
-            <p className="text-2xl pt-6 pb-3">You are registered for this event</p>
+            <p className="text-2xl pt-4 pb-3">You are registered for this event</p>
             {isNotInCalendar ? <Link to={eventLink} target="_blank" className="attend_button">Add to Google calendar</Link> : null}
         </>
     );
