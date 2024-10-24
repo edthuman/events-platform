de91b5e34338306932861f634950b344f10cd93e
edthuman <edwardadirvine@aol.com>
2024-10-24 12:28:40 +0100

    update LogInButton styling

--- src/Header/LogInButton.tsx
+++ src/Header/LogInButton.tsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom"
 
 function LogInButton() {
-    return <Link to="/login" className="flex items-center justify-center w-2/12">
-        <p className="text-xl font-medium text-[#242424] bg-[#8ec584] hover:bg-[#74a06c] p-2">Login</p>
+    return <Link to="/login" className="flex items-start justify-center w-2/12 mt-1 mr-2">
+        <p className="text-xl font-medium text-blue bg-[#8ec584] hover:bg-[#74a06c] p-2 rounded-xl">Login</p>
     </Link>
 }
 
