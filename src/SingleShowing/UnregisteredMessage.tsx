c0bf00e88a27cc5979c401c5794eb6aa20dde7fa
edthuman <edwardadirvine@aol.com>
2024-10-24 11:09:15 +0100

    adjust spacing in UnregisteredMessage

--- src/SingleShowing/UnregisteredMessage.tsx
+++ src/SingleShowing/UnregisteredMessage.tsx
@@ -1,5 +1,5 @@
 function UnregisteredMessage({message}: {message: string}) {
-    return <p className="text-2xl py-6">{message}</p>
+    return <p className="text-2xl mt-4 mb-6">{message}</p>
 }
 
 export default UnregisteredMessage
