69f1ddfed124aec48d9d93d80425a2190eae6fff
edthuman <edwardadirvine@aol.com>
2024-10-24 12:33:51 +0100

    add styling to LogOutButton

--- src/LogOutButton.tsx
+++ src/LogOutButton.tsx
@@ -5,7 +5,9 @@ import { StringStateSetter } from "../types"
 
 function LogOutButton({ setError }: { setError: StringStateSetter }) {
     const { setUser } = useContext(UserContext)
-    return <button onClick={e => handleLogOutClick(e, setError, setUser)}>Log Out</button>
+    return <button className="flex items-start justify-center w-3/12 mt-1 mr-2" onClick={e => handleLogOutClick(e, setError, setUser)}>
+            <p className="text-xl font-medium text-blue bg-[#D45143] hover:bg-[#AF4C40] p-2 rounded-xl">Log Out</p>
+        </button>
 }
 
 export default LogOutButton
