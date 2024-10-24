45fbd23e7ca6d1f498a7c67de04e33a5ffab49ef
edthuman <edwardadirvine@aol.com>
2024-10-24 17:11:55 +0100

    add styling to CheckoutForm

--- src/SingleShowing/CheckoutForm.tsx
+++ src/SingleShowing/CheckoutForm.tsx
@@ -13,7 +13,7 @@ import { handlePayment } from "./event-handlers";
 export default function CheckoutForm({ showing, donation }: { showing: Showing, donation: string }) {
     const stripe = useStripe();
     const elements = useElements();
-    const [message, setMessage] = useState("");
+    const [message, setMessage] = useState("An unexpected error occurred");
     const [isLoading, setIsLoading] = useState(false);
     const { user: { email }} = useContext(UserContext)
     const ticketPrice = showing.price === "any" ? donation : showing.price
@@ -21,15 +21,16 @@ export default function CheckoutForm({ showing, donation }: { showing: Showing, 
   
     return stripe && elements ? (
         <form onSubmit={e => handlePayment(e, stripe, elements, showing.id, setIsLoading, setMessage)}>
+            {message ? <p className="text-xl my-3 py-2 rounded-lg w-10/12 mx-auto bg-red">{message}</p> : null}
             <AddressElement options={{mode: "shipping"}} />
             <LinkAuthenticationElement options={{defaultValues: { email }}} />
             <PaymentElement />
             <button
                 disabled={isLoading || !stripe || !elements}
+                className="text-lg bg-[#30313c] mt-3 rounded-md border border-[#424352] shadow-md shadow-[#202020]"
             >
                 {`Pay £${formattedTicketPrice}`}
             </button>
-            {message ? <p>{message}</p> : null}
         </form>
     ) : null
 }
