03f6db6bd2dba542cfcbc1f0bba1dd69ce12d4f7
edthuman <edwardadirvine@aol.com>
2024-10-25 12:35:41 +0100

    update margin styling in ReturnLinks

--- src/PaymentComplete/ReturnLinks.tsx
+++ src/PaymentComplete/ReturnLinks.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function ReturnLinks({ showingId }:{ showingId: string }) {
     const linkStyling = " text-xl py-3 w-3/4 border hover:text-grey"
     
-    return <div className="flex flex-col gap-5 items-center">
+    return <div className="flex flex-col gap-5 items-center mt-6">
     <Link className={linkStyling} to={`/showing/${showingId}`} target="_self">
         Return to showing page
     </Link>
