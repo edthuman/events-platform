4b291f50c16f3fbb0ab0e5bba67025d1232b737d
edthuman <edwardadirvine@aol.com>
2024-10-25 21:18:15 +0100

    update ToTopButton styling

--- src/ToTopButton.tsx
+++ src/ToTopButton.tsx
@@ -1,6 +1,6 @@
 function ToTopButton() {
     return <button className="text-grey" onClick={() => window.scrollTo(0, 0)}>
-        <p className="text-xl text-off_white w-max mx-auto py-1 px-2 border hover:text-grey">Back to top of page</p>
+        <p className="text-xl text-off_white w-max mx-auto py-2 px-3 border hover:text-grey">Back to top of page</p>
     </button>
 }
 
