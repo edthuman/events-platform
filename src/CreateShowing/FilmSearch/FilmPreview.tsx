212f50f61c0e52b20cfd13c1182998d586ba02eb
edthuman <edwardadirvine@aol.com>
2024-10-25 13:23:38 +0100

    add styling to FilmPreview

--- src/CreateShowing/FilmSearch/FilmPreview.tsx
+++ src/CreateShowing/FilmSearch/FilmPreview.tsx
@@ -1,16 +1,20 @@
 import { FilmPreviewDetails } from "../../../server/omdb-types";
 
 function FilmPreview({ filmDetails }: { filmDetails: FilmPreviewDetails }) {
+    const textStyling = "text-2xl mt-3"
     return (
         <>
-            <p>Details found:</p>
-            <p>Title: {filmDetails.title}</p>
-            <p>Year of Release: {filmDetails.year}</p>
-            <p>Director: {filmDetails.director}</p>
-            <p>Actors: {filmDetails.actors}</p>
+            <div className="mt-6 mb-4">
+                <p className={textStyling + " underline"}>Details found:</p>
+                <p className={textStyling}>Title: {filmDetails.title}</p>
+                <p className={textStyling}>Year of Release: {filmDetails.year}</p>
+                <p className={textStyling}>Director: {filmDetails.director}</p>
+                <p className={textStyling}>Actors: {filmDetails.actors}</p>
+            </div>
             <img
                 src={filmDetails.poster}
                 alt={`Poster for ${filmDetails.title}`}
+                className="mx-auto size-8/12"
             />
         </>
     );
