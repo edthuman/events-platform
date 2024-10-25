7288e734ab0c8f39d5c9e0913d269f8b0749b504
edthuman <edwardadirvine@aol.com>
2024-10-25 19:55:30 +0100

    add target to Link in LoggedOutPayAttempt

--- src/SingleShowing/LoggedOutPayAttempt.tsx
+++ src/SingleShowing/LoggedOutPayAttempt.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 function LoggedOutPayAttempt() {
     return <>
     <h1 className="mt-8 mb-7 px-3 text-2xl">Must be logged in to pay for an event</h1>
-    <Link to="/" className="border px-4 py-2 hover:text-grey">Back to home page</Link>
+    <Link to="/" target="_self" className="border px-4 py-2 hover:text-grey">Back to home page</Link>
 </>
 }
 
