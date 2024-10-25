efe8779286bf5d82a552afc25d817381e1a1d86b
edthuman <edwardadirvine@aol.com>
2024-10-25 11:16:28 +0100

    reorder FilmPreview p tag elements

--- src/CreateShowing/FilmSearch/FilmPreview.tsx
+++ src/CreateShowing/FilmSearch/FilmPreview.tsx
@@ -6,8 +6,8 @@ function FilmPreview({ filmDetails }: { filmDetails: FilmPreviewDetails }) {
             <p>Details found:</p>
             <p>Title: {filmDetails.title}</p>
             <p>Year of Release: {filmDetails.year}</p>
-            <p>Actors: {filmDetails.actors}</p>
             <p>Director: {filmDetails.director}</p>
+            <p>Actors: {filmDetails.actors}</p>
             <img
                 src={filmDetails.poster}
                 alt={`Poster for ${filmDetails.title}`}
