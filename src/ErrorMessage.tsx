1450e9d78847a6b2869ee6d3d8a1480b43b58200
edthuman <edwardadirvine@aol.com>
2024-10-25 20:54:22 +0100

    add ErrorMessage styling

--- src/ErrorMessage.tsx
+++ src/ErrorMessage.tsx
@@ -1,7 +1,7 @@
 function ErrorMessage({ error }: { error: string }) {
     return <>
-        <h1>Error occurred</h1>
-        <p>{error}</p>
+        <h1 className="text-4xl mt-8 mb-6">Error occurred</h1>
+        <p className="text-2xl">{error}</p>
     </>
 }
 
