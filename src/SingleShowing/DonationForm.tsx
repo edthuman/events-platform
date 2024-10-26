8755042aadd05ea62d0972017d937c2a68a63dd9
edthuman <edwardadirvine@aol.com>
2024-10-26 12:38:22 +0100

    update DonationForm input type to text

--- src/SingleShowing/DonationForm.tsx
+++ src/SingleShowing/DonationForm.tsx
@@ -8,7 +8,7 @@ function DonationForm({ donation, setDonation }: {donation: string, setDonation:
     return <>
         <form className="mt-4">
             <label htmlFor="price" className="mr-3">Donation (£):</label>
-            <input id="price" className="w-2/6 text-black pl-1 pr-0.5" type="number" value={donation} placeholder="0" onChange={e => handlePriceInput(e, setDonation, setError)}/>
+            <input id="price" className="w-2/6 text-black pl-1 pr-0.5" type="text" value={donation} placeholder="0" onChange={e => handlePriceInput(e, setDonation, setError)}/>
         </form>
         {error ? <p className="text-xl pt-3">{error}</p> : null}
     </>
