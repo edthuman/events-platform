c506e8bf3e0fd7ae0b1cb7585f4865071e07fc63
edthuman <edwardadirvine@aol.com>
2024-10-27 17:31:56 +0000

    update text colour for accessibility

--- src/Header/LogInButton.tsx
+++ src/Header/LogInButton.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 function LogInButton() {
     return <Link to="/login" className="flex items-start justify-center w-2/12 mt-1 mr-2">
-        <p className="text-xl font-medium text-blue bg-[#8ec584] hover:bg-[#74a06c] p-2 rounded-xl">Login</p>
+        <p className="text-xl font-medium text-[black] bg-[#8ec584] hover:bg-[#74a06c] p-2 rounded-xl">Login</p>
     </Link>
 }
 
