702069f9c5c848830eee447b4ca6ddb266fcd70a
edthuman <edwardadirvine@aol.com>
2024-10-28 12:35:46 +0000

    add larger screen styling for ErrorMessage

--- src/ErrorMessage.tsx
+++ src/ErrorMessage.tsx
@@ -1,7 +1,7 @@
 function ErrorMessage({ error }: { error: string }) {
     return <>
-        <h1 className="text-4xl mt-8 mb-6">Error occurred</h1>
-        <p className="text-2xl">{error}</p>
+        <h1 className="text-4xl lg:text-5xl mt-9 mb-6 lg:mb-8">Error occurred</h1>
+        <p className="text-2xl lg:text-3xl">{error}</p>
     </>
 }
 
