752ffb8eee820be33a9047843ca39685d0348ff1
edthuman <edwardadirvine@aol.com>
2024-10-28 12:25:31 +0000

    add larger screen styling to Loading

--- src/Loading.tsx
+++ src/Loading.tsx
@@ -1,6 +1,6 @@
 function Loading() {
-    return <div className="py-20">
-        <p className="text-3xl">Loading...</p>
+    return <div className="py-10">
+        <p className="text-4xl xl:text-5xl lg:text-off_white">Loading...</p>
     </div>
 }
 
