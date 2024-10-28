9611431723af9645373ff269534cfdf9ecf275f3
edthuman <edwardadirvine@aol.com>
2024-10-28 12:26:33 +0000

    add larger screen styling to HomeLink

--- src/SingleShowing/HomeLink.tsx
+++ src/SingleShowing/HomeLink.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 
 function HomeLink() {
-    return <Link to="/" className="block text-left hover:text-grey mt-2">
+    return <Link to="/" className="block text-left hover:text-grey mt-2 xl:text-lg">
         {"←"}<span className="underline hover:no-underline">Back to all showings</span>
     </Link>
 }
