a06c1ca98f5cd9f5846b9195e728b782bb67d87e
edthuman <edwardadirvine@aol.com>
2024-10-28 14:53:00 +0000

    add larger screen styling to ReturnLinks

--- src/PaymentComplete/ReturnLinks.tsx
+++ src/PaymentComplete/ReturnLinks.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 function ReturnLinks({ showingId }:{ showingId: any }) {
-    const linkStyling = " text-xl py-3 w-3/4 border hover:text-grey"
+    const linkStyling = "text-xl py-3 md:py-4 xl:py-5 w-9/12 sm:w-7/12 md:w-5/12 lg:w-4/12 border hover:text-grey"
     
     return <div className="flex flex-col gap-5 items-center mt-6">
     <Link className={linkStyling} to={`/showing/${showingId}`} target="_self">
