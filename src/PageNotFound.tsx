199ff58ba62d5bb19cb4483f7f43c95ccda19ec4
edthuman <edwardadirvine@aol.com>
2024-10-30 12:42:30 +0000

    add PageNotFound component

---
+++ src/PageNotFound.tsx
@@ -0,0 +1,10 @@
+import HomeLink from "./SingleShowing/HomeLink"
+
+function PageNotFound() {
+    return <>
+        <HomeLink/>
+        <h1 className="text-3xl md:text-4xl xl:text-5xl mt-10">Page not found</h1>
+    </>
+}
+
+export default PageNotFound
