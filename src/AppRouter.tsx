72e48be62696109a851bf338bb06f95e391d7aa6
edthuman <edwardadirvine@aol.com>
2024-10-30 12:43:16 +0000

    add PageNotFound to '/*' route in AppRouter

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -25,6 +25,10 @@ const router = createBrowserRouter([
     {
         path: "/complete",
         element: <Page page="PaymentComplete" />,
+    },
+    {
+        path: "/*",
+        element: <Page page="PageNotFound" />
     }
 ]);
 
