eda091a5d56fd80dc514c609ced390e5cd84c84d
edthuman <edwardadirvine@aol.com>
2024-11-11 14:51:55 +0000

    update EmailLogin to use update handleEmailLogin parameters

--- src/Login/EmailLogin.tsx
+++ src/Login/EmailLogin.tsx
@@ -3,12 +3,15 @@ import UserContext from "../../hooks/UserContext";
 import EmailFormElements from "./EmailFormElements";
 import { handleEmailLogin } from "./event-handlers";
 import { StringStateSetter } from "../../types";
+import FirebaseContext from "../../hooks/FirebaseContext";
 
-function EmailLogin({ setError }: { setError: StringStateSetter }) {
+function EmailLogin({ setError, role }: { setError: StringStateSetter, role: string }) {
     const { setUser } = useContext(UserContext);
+    const { firebase } = useContext(FirebaseContext)
+    const databaseURL = import.meta.env.VITE_FIREBASE_REALTIME_URL
 
     return <>
-        <form className="flex flex-col" onSubmit={e => handleEmailLogin(e, setUser, setError)}>
+        <form className="flex flex-col" onSubmit={e => handleEmailLogin(e, setUser, setError, role, firebase, databaseURL)}>
             <EmailFormElements />
             <button type="submit" className="mx-auto mt-5 hover:text-grey">
                 <p className="text-base border p-2 w-max mx-auto md:text-lg md:px-3">Sign in</p>
