46ed365c175258d6de8663b93b8aa0cd2467fd26
edthuman <edwardadirvine@aol.com>
2024-11-12 14:37:07 +0000

    update ShowingCard styling to handle text overflow

--- src/Showings/ShowingCard.tsx
+++ src/Showings/ShowingCard.tsx
@@ -8,10 +8,10 @@ function ShowingCard({showing}: { showing: Showing }) {
 
     return <Link to={`/showing/${showing.id}`} target="_self">
         <div className="mb-1 flex flex-col items-center py-4 bg-[#23658e] drop-shadow-xl hover:bg-[#66889c] rounded-lg">
-            <div className="flex justify-center h-10/12">
-                <img src={`${showing.poster}`} alt={`Movie poster for ${showing.film}`} className="w-10/12 py-auto"/>
+            <div className="flex justify-center px-5">
+                <img src={`${showing.poster}`} alt={`Movie poster for ${showing.film}`} className="w-10/12 w-[15.5rem] py-auto"/>
             </div>
-            <p className="text-2.5xl pt-2">{showing.name}</p>
+            <p className="text-2.5xl px-4 pt-2 w-72 overflow-hidden text-nowrap text-ellipsis">{showing.name}</p>
             <p className="text-2.5xl">{day}</p>
         </div>
     </Link>
