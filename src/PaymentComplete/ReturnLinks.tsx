cfdfa8ce53239ef070ebd3fcff54c2e9d46028f0
edthuman <edwardadirvine@aol.com>
2024-11-13 15:08:56 +0000

    revert section in ReturnLinks to a div

--- src/PaymentComplete/ReturnLinks.tsx
+++ src/PaymentComplete/ReturnLinks.tsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 function ReturnLinks({ showingId }:{ showingId: any }) {
     const linkStyling = "text-xl py-3 md:py-4 xl:py-5 w-9/12 sm:w-7/12 md:w-5/12 lg:w-4/12 border hover:text-grey"
     
-    return <section className="flex flex-col gap-5 items-center mt-6">
+    return <div className="flex flex-col gap-5 items-center mt-6">
         <Link className={linkStyling} to={`/showing/${showingId}`} target="_self">
             Return to showing page
         </Link>
         <Link className={linkStyling} to="/" target="_self">
             Return to home page
         </Link>
-    </section>
+    </div>
 }
 
 export default ReturnLinks
