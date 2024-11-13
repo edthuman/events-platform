2c6518cd20a43d7003e93286d8f85cb1ec9da397
edthuman <edwardadirvine@aol.com>
2024-11-13 14:14:19 +0000

    remove unecessary fragment from SignUpForm.tsx

--- src/Login/SignUpForm.tsx
+++ src/Login/SignUpForm.tsx
@@ -7,17 +7,12 @@ import { StringStateSetter } from "../../types";
 function SignUpForm({ setError }: { setError: StringStateSetter }) {
     const { setUser } = useContext(UserContext);
 
-    return <>
-        <form
-            onSubmit={(e) => handleSignUpClick(e, setUser, setError)}
-            className="flex flex-col"
-        >
-            <EmailFormElements />
-            <button type="submit" className="w-max h-max mx-auto mt-5 hover:text-grey">
-                <p className="text-base border p-2 w-max mx-auto md:text-lg md:px-3">Sign Up</p>
-            </button>
-        </form>
-    </>
+    return <form onSubmit={(e) => handleSignUpClick(e, setUser, setError)} className="flex flex-col">
+        <EmailFormElements />
+        <button type="submit" className="w-max h-max mx-auto mt-5 hover:text-grey">
+            <p className="text-base border p-2 w-max mx-auto md:text-lg md:px-3">Sign Up</p>
+        </button>
+    </form>
 }
 
 export default SignUpForm;
