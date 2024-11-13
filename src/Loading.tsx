961e34dc8c9ed8c0e13f1c11b465568f49e8ab5d
edthuman <edwardadirvine@aol.com>
2024-11-13 14:47:59 +0000

    remove unnecessary div from Loading.tsx

--- src/Loading.tsx
+++ src/Loading.tsx
@@ -1,7 +1,5 @@
 function Loading() {
-    return <div className="py-10">
-        <p className="text-4xl xl:text-5xl lg:text-off_white">Loading...</p>
-    </div>
+    return <p className="text-4xl xl:text-5xl lg:text-off_white py-10">Loading...</p>
 }
 
 export default Loading
