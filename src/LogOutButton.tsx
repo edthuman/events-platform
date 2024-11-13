7d2ba94d729577d63d0daa58fc02d2861bebec02
edthuman <edwardadirvine@aol.com>
2024-11-13 12:27:19 +0000

    update sizing of LogOutButton

--- src/LogOutButton.tsx
+++ src/LogOutButton.tsx
@@ -7,7 +7,7 @@ function LogOutButton({ setError }: { setError: StringStateSetter }) {
     const { setUser } = useContext(UserContext);
     return (
         <button
-            className="flex items-start justify-center w-3/12 mt-1 mr-2"
+            className="flex items-start justify-center w-max h-max mt-1 mr-4"
             onClick={(e) => handleLogOutClick(e, setError, setUser)}
         >
             <p className="text-xl font-medium text-black bg-[#D45143] hover:bg-[#AF4C40] p-2 rounded-xl md:text-2xl lg:p-3">
