c60d0cefb9f248095fd96960f1838f7042c818c8
edthuman <edwardadirvine@aol.com>
2024-11-13 14:57:10 +0000

    add main tags to PageNotFound.tsx

--- src/PageNotFound.tsx
+++ src/PageNotFound.tsx
@@ -1,10 +1,10 @@
 import HomeLink from "./SingleShowing/HomeLink"
 
 function PageNotFound() {
-    return <>
+    return <main>
         <HomeLink/>
         <h1 className="text-3xl md:text-4xl xl:text-5xl mt-10">Page not found</h1>
-    </>
+    </main>
 }
 
 export default PageNotFound
